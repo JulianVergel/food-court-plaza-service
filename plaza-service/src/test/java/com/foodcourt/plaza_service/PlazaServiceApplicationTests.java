9d9d8a5ff56b97af489d3a9b62d4eb19f9bd651a
JaVergelVe <javergelve@ufpso.edu.co>
2025-07-10 15:41:12 -0500

    Primer commit

---
+++ plaza-service/src/test/java/com/foodcourt/plaza_service/PlazaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcourt.plaza_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
