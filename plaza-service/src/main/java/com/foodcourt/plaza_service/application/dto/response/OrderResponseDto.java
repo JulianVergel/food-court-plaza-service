4f9100630ab9a67b70e5e99974f1b23066778865
JulianVergel <julian.vergel@pragma.com.co>
2025-07-22 15:28:59 -0500

    Arreglando cosas de la logica para que todo funciones bien

--- plaza-service/src/main/java/com/foodcourt/plaza_service/application/dto/response/OrderResponseDto.java
+++ plaza-service/src/main/java/com/foodcourt/plaza_service/application/dto/response/OrderResponseDto.java
@@ -1,7 +1,12 @@
 package com.foodcourt.plaza_service.application.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class OrderResponseDto {
     private Long id;
     private Long customerId;
